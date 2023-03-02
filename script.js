62f91e1f0a1b78709d5eb38be744289fa4947037
ryangholland <rghcode@gmail.com>
2023-03-02 18:57:45 -0500

    Add mouseover fill effect to each box

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     let newSquare = document.createElement("div");
     newSquare.classList.add("square");
+
+    newSquare.addEventListener("mouseover", (e) => {
+      e.target.classList.add("filled");
+    });
+
     newRow.append(newSquare);
   }
   container.append(newRow);
