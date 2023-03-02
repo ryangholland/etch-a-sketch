e82a009777024a4144b988544888da766814567a
ryangholland <rghcode@gmail.com>
2023-03-02 18:53:30 -0500

    Create a 16x16 grid with JS

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const container = document.getElementById("container");
 
-container.textContent = "sup world"
+for (let i = 0; i < 16; i++) {
+  let newRow = document.createElement("div");
+  newRow.classList.add("row");
+  for (let j = 0; j < 16; j++) {
+    let newSquare = document.createElement("div");
+    newSquare.classList.add("square");
+    newRow.append(newSquare);
+  }
+  container.append(newRow);
+}
