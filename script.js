2a13f7bdf568e8bce6690acb10266695364573a1
ryangholland <rghcode@gmail.com>
2023-03-04 10:02:34 -0500

    Implement drawGrid function to build a grid using a custom box size

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const container = document.getElementById("container");
+const container = document.getElementById("grid-container");
+const sizeBtn = document.getElementById("size-btn");
 
-for (let i = 0; i < 16; i++) {
-  let newRow = document.createElement("div");
-  newRow.classList.add("row");
-  for (let j = 0; j < 16; j++) {
-    let newSquare = document.createElement("div");
-    newSquare.classList.add("square");
+function drawGrid(size) {
+  for (let i = 0; i < 16; i++) {
+    let newRow = document.createElement("div");
+    newRow.classList.add("row");
+    for (let j = 0; j < 16; j++) {
+      let newSquare = document.createElement("div");
+      newSquare.classList.add("square");
+      newSquare.style.height = `${size}px`;
+      newSquare.style.width = `${size}px`;
+      newSquare.addEventListener("mouseover", (e) => {
+        e.target.classList.add("filled");
+      });
 
-    newSquare.addEventListener("mouseover", (e) => {
-      e.target.classList.add("filled");
-    });
-
-    newRow.append(newSquare);
+      newRow.append(newSquare);
+    }
+    container.append(newRow);
   }
-  container.append(newRow);
 }
+
+sizeBtn.addEventListener("click", () => {
+  let squareCount = prompt("Squares per row:");
+});
+
+drawGrid(50);
